4ec155c11edf641c139f9bba16227f24432c6af2
Damian Krzemiński <damian.krzeminski13@gmail.com>
2019-11-19 16:33:50 +0100

    Adding Menu

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Project</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    </head>
+    <body>
+    <div class="container-fluid">
+        <div class="row-content">
+           <div class="col-sm-4">
+                <h1>Project</h1>
+
+                <h2>Content of file</h2>
+                <h2>Number of letters in the file</h2>
+                <h2>Number of words in the file</h2>
+                <h2>Number of punctuation marks in the file</h2>
+                <h2>Number of sentences in the file</h2>
+                <h2>Report on the use of letters A-Z</h2>
+                <!-- SAVE IN STATYSTYKI.TXT-->
+            </div>
+        </div>
+    </div>
+    <footer class="container-fluid">
+        <p>Project by Damian Krzemiński, Piotr Urban and Patryk Kaźmierczak</p>
+    </footer>
+</body>
+</html>
